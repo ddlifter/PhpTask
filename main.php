b478be02949c161671b6444c9697bbeaf65d4211
ddlifter <butorin_kirya@inbox.ru>
2024-05-19 15:14:22 +0300

    feat(main): count of veterans for all sports

--- main.php
+++ main.php
@@ -21,35 +21,34 @@
 <h2>Данные о ветеранах</h2>
 <?php
 // Подключение к базе данных
-$dbconn = pg_connect("host=localhost dbname=php user=postgres password=12345")
-or die('Could not connect: ' . pg_last_error());
+$dbconn = pg_connect("host=localhost dbname=php user=postgres password=12345") or die('Could not connect: '. pg_last_error());
 
 // Запрос для получения данных о ветеранах
 $query = "SELECT * FROM veterans";
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+$result = pg_query($query) or die('Query failed: '. pg_last_error());
 
 // Вывод данных в виде таблицы
 echo "<table>";
 echo "<tr><th>ID</th><th>Фамилия</th><th>Имя</th><th>Отчество</th><th>Город</th><th>Вид спорта</th><th>Возрастная группа</th></tr>";
 while ($row = pg_fetch_assoc($result)) {
     echo "<tr>";
-    echo "<td>" . $row['id_veteran'] . "</td>";
-    echo "<td>" . $row['surname'] . "</td>";
-    echo "<td>" . $row['name'] . "</td>";
-    echo "<td>" . $row['thirdname'] . "</td>";
-    echo "<td>" . $row['city'] . "</td>";
+    echo "<td>". $row['id_veteran']. "</td>";
+    echo "<td>". $row['surname']. "</td>";
+    echo "<td>". $row['name']. "</td>";
+    echo "<td>". $row['thirdname']. "</td>";
+    echo "<td>". $row['city']. "</td>";
 
     // Запрос для получения названия вида спорта
-    $sport_query = "SELECT name FROM sports WHERE id_sport = " . $row['id_sport'];
+    $sport_query = "SELECT name FROM sports WHERE id_sport = ". $row['id_sport'];
     $sport_result = pg_query($sport_query);
     $sport_row = pg_fetch_assoc($sport_result);
-    echo "<td>" . $sport_row['name'] . "</td>";
+    echo "<td>". $sport_row['name']. "</td>";
 
     // Запрос для получения названия возрастной группы
-    $age_group_query = "SELECT age_group FROM age_groups WHERE id_age_group = " . $row['id_age_group'];
+    $age_group_query = "SELECT age_group FROM age_groups WHERE id_age_group = ". $row['id_age_group'];
     $age_group_result = pg_query($age_group_query);
     $age_group_row = pg_fetch_assoc($age_group_result);
-    echo "<td>" . $age_group_row['age_group'] . "</td>";
+    echo "<td>". $age_group_row['age_group']. "</td>";
 
     echo "</tr>";
 }
@@ -72,16 +71,15 @@ pg_close($dbconn);
     <select name="sport">
         <?php
         // Подключение к базе данных
-        $dbconn = pg_connect("host=localhost dbname=php user=postgres password=12345")
-        or die('Could not connect: ' . pg_last_error());
+        $dbconn = pg_connect("host=localhost dbname=php user=postgres password=12345") or die('Could not connect: '. pg_last_error());
 
         // Запрос для получения всех видов спорта
         $sport_query = "SELECT * FROM sports";
-        $sport_result = pg_query($sport_query) or die('Query failed: ' . pg_last_error());
+        $sport_result = pg_query($sport_query) or die('Query failed: '. pg_last_error());
 
         // Вывод вариантов в выпадающий список
         while ($row = pg_fetch_assoc($sport_result)) {
-            echo "<option value='" . $row['id_sport'] . "'>" . $row['name'] . "</option>";
+            echo "<option value='". $row['id_sport']. "'>". $row['name']. "</option>";
         }
 
         // Освобождаем результат запроса
@@ -95,16 +93,15 @@ pg_close($dbconn);
     <select name="age_group">
         <?php
         // Подключение к базе данных
-        $dbconn = pg_connect("host=localhost dbname=php user=postgres password=12345")
-        or die('Could not connect: ' . pg_last_error());
+        $dbconn = pg_connect("host=localhost dbname=php user=postgres password=12345") or die('Could not connect: '. pg_last_error());
 
         // Запрос для получения всех возрастных групп
         $age_group_query = "SELECT * FROM age_groups";
-        $age_group_result = pg_query($age_group_query) or die('Query failed: ' . pg_last_error());
+        $age_group_result = pg_query($age_group_query) or die('Query failed: '. pg_last_error());
 
         // Вывод вариантов в выпадающий список
         while ($row = pg_fetch_assoc($age_group_result)) {
-            echo "<option value='" . $row['id_age_group'] . "'>" . $row['age_group'] . "</option>";
+            echo "<option value='". $row['id_age_group']. "'>". $row['age_group']. "</option>";
         }
 
         // Освобождаем результат запроса
@@ -116,5 +113,40 @@ pg_close($dbconn);
     </select><br>
     <input type="submit" value="Добавить ветерана">
 </form>
+
+<h2>Число ветеранов по каждому виду спорта</h2>
+<form method="post" action="">
+    <input type="hidden" name="action" value="getSportVeteransCount">
+    <button type="submit">Получить данные</button>
+</form>
+
+<?php
+if (isset($_POST['action']) && $_POST['action'] == 'getSportVeteransCount') {
+    // Подключение к базе данных
+    $dbconn = pg_connect("host=localhost dbname=php user=postgres password=12345") or die('Could not connect: '. pg_last_error());
+
+    // SQL-запрос для получения количества ветеранов по каждому виду спорта
+    $query = "SELECT s.name AS sport, COUNT(v.id_veteran) AS veteran_count FROM sports s LEFT JOIN veterans v ON s.id_sport = v.id_sport GROUP BY s.name;";
+    $result = pg_query($query) or die('Query failed: '. pg_last_error());
+
+    // Вывод результата в виде таблицы
+    echo "<table>";
+    echo "<tr><th>Спорт</th><th>Количество ветеранов</th></tr>";
+    while ($row = pg_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>". $row['sport']. "</td>";
+        echo "<td>". $row['veteran_count']. "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    // Освобождаем результат запроса
+    pg_free_result($result);
+
+    // Закрываем соединение с базой данных
+    pg_close($dbconn);
+}
+?>
+
 </body>
 </html>
